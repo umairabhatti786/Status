f4825278fc22a6b22f21249ca30a78ed859153b7
talhaansari77 <talhaprince09@gmail.com>
2024-05-22 23:45:17 +0300

    likeButton

---
+++ src/components/LikeButton/LikeButton.tsx
@@ -0,0 +1,50 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { scale, verticalScale } from "react-native-size-matters";
+import { colors } from "../../utils/colors";
+import CustomText from "../CustomText";
+
+const LikeButton = ({ likes_count, onPress }: any) => {
+  const [likes, setLikes] = useState(likes_count);
+  const [liked, setLiked] = useState(false);
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        if (liked) {
+          setLiked(false);
+          setLikes(likes - 1);
+        } else {
+          setLiked(true);
+          setLikes(likes + 1);
+        }
+        onPress();
+      }}
+      style={{
+        paddingHorizontal: scale(10),
+        // paddingVertical: verticalScale(2),
+        position: "absolute",
+        bottom: verticalScale(10),
+        left: scale(25),
+        backgroundColor: colors.black300,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: scale(20),
+        borderWidth: 1,
+        borderColor: colors.black,
+      }}
+    >
+      <CustomText
+        color={colors.grey300}
+        size={15}
+        // fontFam="Inter-Medium"
+        style={{ letterSpacing: 3 }}
+        text={"❤️" + "  " + likes}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export default LikeButton;
+
+const styles = StyleSheet.create({});
